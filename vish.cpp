0b60f2dc8dacedff937c5a25151c4aa8bf8ed705
Gustavo Jácome <155338841+gustavofjacome@users.noreply.github.com>
2026-09-10 21:18:03 -0300

    docs: adiciona comentários com o que eu entendi de cada linha

--- vish.cpp
+++ vish.cpp
@@ -3,53 +3,48 @@
 #include <sys/wait.h>
 
 void process_command(std::string command) {
-    // Se for comando interno...
+    // comandos internos
     if (command == "exit")
         exit(0);
 
-    // Se for comando externo
-    // * necessário verificar se é para ser executado em background
-    /*
-        Se for caminho relativo, procurar o comando na lista de diretórios
-        Se for absoluto verifica se comando existe
-    */
-
+   
+    // caminho onde serão procurados os binarios /bin/comando
     std::string absolute_path = "/bin/" + command;
 
-    if (access(absolute_path.c_str(), F_OK) == 0) {
-        if (access(absolute_path.c_str(), X_OK) == 0) {
-            pid_t pid = fork();
+    if (access(absolute_path.c_str(), F_OK) == 0) { // existe o arquivo?
+        if (access(absolute_path.c_str(), X_OK) == 0) { // esse arquivo que existe tem permissão para executar?
+            pid_t pid = fork(); // cria o clone
 
-            if (pid < 0) {
+            if (pid < 0) { // o fork retorna 0 ou 1 se deu menor que 0 aconteceu erro
                 // Erro
                 std::cout << "Erro de execução!" << std::endl;
                 return;
-            } else if (pid == 0) {
+            } else if (pid == 0) { // se ele retornou 0 é o filho
                 char* argv[2] = {
-                    (char*)command.c_str(),
+                    (char*)command.c_str(), // como as chamadas de sistemas foram feitas em C temos que transformar a string em um array de char e é isso que o c_str faz transforma o std::string em uma string C
                     nullptr
                 };
 
-                execve(absolute_path.c_str(), argv, NULL);
+                execve(absolute_path.c_str(), argv, NULL); // o filho vai rodar o /bin/comando no outro fork 
             } else {
-                waitpid(pid, nullptr, 0);
+                waitpid(pid, nullptr, 0); // enquanto o isso o pai so espera o filho terminar
             }
         } else {
-            std::cout << "permission denied: " << command << std::endl;
+            std::cout << "permission denied: " << command << std::endl; // se nao tem permissão
         }
     } else {
-        std::cout << "Command not found: " << command << std::endl;
+        std::cout << "Command not found: " << command << std::endl; // se nao existe
     }
 }
 
 int main() {
     while (true) {
-        std::cout << "$> ";
+        std::cout << "vi$h> "; // acho que esse nome ta muito grande
 
         std::string command;
-        getline(std::cin, command);
+        getline(std::cin, command); // pega a linha inteira, ou seja até o ENTER
 
-        process_command(command);
+        process_command(command); // chama a função la de cima
     }
 
     return 0;
