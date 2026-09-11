e94df938154c0644917053c4dcce7384adf7c382
Gustavo Jácome <155338841+gustavofjacome@users.noreply.github.com>
2026-09-10 22:06:45 -0300

    feat: modifiquei para aceitar rodar arquivos da /bin/temp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+  int a, b;
+
+  a = 1;
+  b = 6;
+
+  std::cout << a + b << '\n';
+  
+  return 0;
+}
--- vish.cpp
+++ vish.cpp
@@ -10,22 +10,27 @@ void process_command(std::string command) {
    
     // caminho onde serão procurados os binarios /bin/comando
     std::string absolute_path = "/bin/" + command;
+    std::string temp_path = "/bin/temp/" + command;
 
-    if (access(absolute_path.c_str(), F_OK) == 0) { // existe o arquivo?
-        if (access(absolute_path.c_str(), X_OK) == 0) { // esse arquivo que existe tem permissão para executar?
+    if (access(absolute_path.c_str(), F_OK) == 0 || access(temp_path.c_str(), F_OK) == 0) { // existe o arquivo?
+        if (access(absolute_path.c_str(), X_OK) == 0 || access(temp_path.c_str(), X_OK) == 0) { // esse arquivo que existe tem permissão para executar?
             pid_t pid = fork(); // cria o clone
 
-            if (pid < 0) { // o fork retorna 0 ou 1 se deu menor que 0 aconteceu erro
-                // Erro
-                std::cout << "Erro de execução!" << std::endl;
-                return;
-            } else if (pid == 0) { // se ele retornou 0 é o filho
-                char* argv[2] = {
-                    (char*)command.c_str(), // como as chamadas de sistemas foram feitas em C temos que transformar a string em um array de char e é isso que o c_str faz transforma o std::string em uma string C
-                    nullptr
-                };
-
-                execve(absolute_path.c_str(), argv, NULL); // o filho vai rodar o /bin/comando no outro fork 
+        if (pid < 0) { // o fork retorna 0 ou 1 se deu menor que 0 aconteceu erro
+            // Erro
+            std::cout << "Erro de execução!" << std::endl;
+            return;
+        } else if (pid == 0) { // se ele retornou 0 é o filho
+            char* argv[2] = {
+                (char*)command.c_str(), // como as chamadas de sistemas foram feitas em C temos que transformar a string em um array de char e é isso que o c_str faz transforma o std::string em uma string C
+                nullptr
+            };
+                if (access(absolute_path.c_str(), F_OK) == 0)
+                {
+                    execve(absolute_path.c_str(), argv, NULL); // o filho vai rodar o /bin/comando no outro fork 
+                } else {
+                    execve(temp_path.c_str(), argv, NULL);
+                }
             } else {
                 waitpid(pid, nullptr, 0); // enquanto o isso o pai so espera o filho terminar
             }
