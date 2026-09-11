2bec6222759c69502b7652ca73e9b1f5cb7b54d4
Gustavo Jácome <155338841+gustavofjacome@users.noreply.github.com>
2026-09-10 21:10:24 -0300

    docs: retira os comentarios do professor

--- vish.cpp
+++ vish.cpp
@@ -17,9 +17,7 @@ void process_command(std::string command) {
     std::string absolute_path = "/bin/" + command;
 
     if (access(absolute_path.c_str(), F_OK) == 0) {
-        // Arquivo existe no diretório
         if (access(absolute_path.c_str(), X_OK) == 0) {
-            // Arquivo é executável
             pid_t pid = fork();
 
             if (pid < 0) {
@@ -27,7 +25,6 @@ void process_command(std::string command) {
                 std::cout << "Erro de execução!" << std::endl;
                 return;
             } else if (pid == 0) {
-                // Processo filho
                 char* argv[2] = {
                     (char*)command.c_str(),
                     nullptr
@@ -35,23 +32,12 @@ void process_command(std::string command) {
 
                 execve(absolute_path.c_str(), argv, NULL);
             } else {
-                // Processo pai
-                /*
-                    Deve adicionar processo filho na lista (std::vector)
-                    de processos em execução para gerenciar background.
-                */
-
-                /*
-                    Processo pai espera processo filho terminar.
-                */
                 waitpid(pid, nullptr, 0);
             }
         } else {
-            // Arquivo não é executável
             std::cout << "permission denied: " << command << std::endl;
         }
     } else {
-        // Arquivo não existe
         std::cout << "Command not found: " << command << std::endl;
     }
 }
